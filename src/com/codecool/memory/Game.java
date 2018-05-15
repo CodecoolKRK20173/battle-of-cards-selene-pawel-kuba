aba5e86fb41e2004000db3feda0f3e36415195de
Jakub Wojtasik <jakubwojtasikzsp@gmail.com>
2018-05-15 11:03:54 +0200

    Add main window changed table color

---
+++ src/com/codecool/memory/Game.java
@@ -0,0 +1,20 @@
+package com.codecool.memory;
+
+import javafx.scene.image.Image;
+import javafx.scene.layout.*;
+
+public class Game extends Pane {
+
+  public Game() {}
+
+  public void setTableBackground(Image tableBackground) {
+    setBackground(
+        new Background(
+            new BackgroundImage(
+                tableBackground,
+                BackgroundRepeat.REPEAT,
+                BackgroundRepeat.REPEAT,
+                BackgroundPosition.CENTER,
+                BackgroundSize.DEFAULT)));
+  }
+}
--- src/com/codecool/memory/Memory.java
+++ src/com/codecool/memory/Memory.java
@@ -1,1 +1,24 @@
 package com.codecool.memory;
+
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.image.Image;
+import javafx.stage.Stage;
+
+public class Memory extends Application {
+  private final double WINDOW_WIDTH = 1400;
+  private final double WINDOW_HEIGHT = 900;
+
+  public static void main(String[] args) {
+    launch(args);
+  }
+
+  @Override
+  public void start(Stage primaryStage) {
+    Game game = new Game();
+    game.setTableBackground(new Image("table/green.png"));
+    primaryStage.setTitle("Memory Game");
+    primaryStage.setScene(new Scene(game, WINDOW_WIDTH, WINDOW_HEIGHT));
+    primaryStage.show();
+  }
+}
