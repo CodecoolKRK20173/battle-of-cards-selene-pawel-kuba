a4dab14fd885131e6decee6e2e2f3d0e89399be2
Paul <bielcu@gmail.com>
2018-05-15 13:31:28 +0200

    move images to proper folder, implement Game class

---
+++ src/com/codecool/memory/Card.java
@@ -0,0 +1,26 @@
+public class Card extends ImageView {
+  private int cardName;
+  private boolean isFaceUp = false;
+  private Image frontImage;
+  private Image backImage;
+
+  public Card(int name) {
+    this.cardName = cardName;
+  }
+
+  public boolean getIsFaceUp() {
+    return isFaceUp;
+  }
+
+  public int getName() {
+    return cardName;
+  }
+
+  public setIsFaceUp() {
+    if (isFaceUp) {
+      isFaceUp = false;
+    } else {
+      isFaceUp = true;
+    }
+  }
+}
