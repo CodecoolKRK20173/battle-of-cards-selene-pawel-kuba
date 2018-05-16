ecaae0f8dfbc6ff9b3c87abb0b8d7d9fd772a90d
Jakub Wojtasik <jakubwojtasikzsp@gmail.com>
2018-05-16 11:40:33 +0200

    Add setCardsOnTable() Modify initPiles()

--- src/com/codecool/memory/Game.java
+++ src/com/codecool/memory/Game.java
@@ -1,18 +1,28 @@
 package com.codecool.memory;
 
+import javafx.collections.ObservableList;
 import javafx.scene.image.Image;
 import javafx.scene.layout.*;
+import javafx.scene.layout.Pane;
 
 public class Game extends Pane {
+  public Pile stock;
+  private Pile playerPile;
 
   public Game() {
     initPiles();
+    System.out.println(stock.getCards());
+    setCardsOnTable();
   }
 
   public void initPiles() {
+    stock = new Pile("stock", 10);
+    Card.createStartPile(stock, 32);
 
-    Pile stock = new Pile("stock", 10);
-    getChildren().add(stock);
+    playerPile = new Pile("playerPile", 0);
+    playerPile.setBlurredBackground();
+    playerPile.setLayoutX(95);
+    playerPile.setLayoutY(20);
   }
 
   public void setTableBackground(Image tableBackground) {
@@ -25,4 +35,18 @@ public class Game extends Pane {
                 BackgroundPosition.CENTER,
                 BackgroundSize.DEFAULT)));
   }
+
+  public void setCardsOnTable() {
+    ObservableList<Card> cards = stock.getCards();
+    int j = 0;
+    for (int i = 0; i < 32; i++) {
+      Card card = cards.get(i);
+      card.setLayoutX(20 * (i + 1));
+      card.setLayoutY(20 * (j + 1));
+      if (i % 8 == 0) j++;
+      getChildren().add(card);
+    }
+  }
+
+  public void addCard() {}
 }
