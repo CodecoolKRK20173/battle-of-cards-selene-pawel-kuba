96f04283f6c1e6e7f7a864decbbc3805e94a11f9
Paul <bielcu@gmail.com>
2018-05-16 15:05:54 +0200

    change green background image, set default bacground to green, scale red background

--- src/com/codecool/memory/Memory.java
+++ src/com/codecool/memory/Memory.java
@@ -4,28 +4,19 @@ import javafx.application.Application;
 import javafx.scene.Scene;
 import javafx.scene.image.Image;
 import javafx.stage.Stage;
-import javafx.scene.media.Media;
-import javafx.scene.media.MediaPlayer;
-import java.io.File;
 
 public class Memory extends Application {
   private final double WINDOW_WIDTH = 1400;
   private final double WINDOW_HEIGHT = 900;
-  private final String musicFile = "resources/Music/lala.mp3";
-  private Media sound = new Media(new File(musicFile).toURI().toString());
-  private MediaPlayer mediaPlayer = new MediaPlayer(sound);
+
   public static void main(String[] args) {
     launch(args);
   }
 
   @Override
   public void start(Stage primaryStage) {
-    mediaPlayer.setCycleCount(MediaPlayer.INDEFINITE);
-    mediaPlayer.play();
-    mediaPlayer.setVolume(0.5);
-
     Game game = new Game();
-    game.setTableBackground(new Image("table/red.png"));
+    game.setTableBackground(new Image("table/green.png"));
     primaryStage.setTitle("Memory Game");
     primaryStage.setScene(new Scene(game, WINDOW_WIDTH, WINDOW_HEIGHT));
     primaryStage.show();
